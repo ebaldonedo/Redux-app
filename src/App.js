5d3eeaca264f4ec0bc1465a7f425f9141ed997ac
Enmanuel Baldonedo <ebaldonedo@hotmail.com>
2022-09-26 18:07:01 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from 'react';
 
 
 import Header from './components/Header';
 
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
+import Producto from './components/Producto';
+import NuevoProducto from './components/NuevoProducto';
+
 function App() {
   return (
     <div>
-      <Header/>
+     
+        <Header/>
+
+        <div className="container">
+        <Router>
+              <Routes>
+                <Route path="/" element={<Producto />} />
+                <Route path="/productos/nuevo" element={<NuevoProducto/>} />
+                
+              </Routes>
+            </Router>
+        </div>
+      
+      
     </div>
   );
 }
